c4e97dc6f78b81b09776fd9ad0c011cc423f9640
rhinoloige <rhinoloige@mail.ru>
2023-03-08 15:45:01 +0300

    new format

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,10 +3,16 @@
 
 
 int main() {
+<<<<<<< HEAD
 //enter my name
+=======
+//enter your name
+>>>>>>> 5502aa8 (new format)
 std::cout << "Enter your name" << std::endl;
 std::string name = "";
 std::cin >> name;
 std::cout << "Hello world from " + name  << std::endl;
 return 1;
 }
+
+
