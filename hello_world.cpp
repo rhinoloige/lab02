b5356c29e5564c9195c6c0ffaa4a2e8eb6531d49
rhinoloige <rhinoloige@mail.ru>
2023-03-08 15:04:03 +0300

    without namespace

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
+
 int main() {
-cout << "Enter your name" << endl;
-string name = "";
-cin >> name;
-cout << "Hello world from " + name  << endl;
+std::cout << "Enter your name" << std::endl;
+std::string name = "";
+std::cin >> name;
+std::cout << "Hello world from " + name  << std::endl;
 return 1;
 }
